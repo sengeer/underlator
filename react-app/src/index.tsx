faec294e02aa6eec29cab5814dd4f9891d44ccd1
Sengeer <sengeer@mail.ru>
2025-01-05 16:11:02 +0300

    chore: install and integrate TypeScript

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './app/styles/root.scss';
 import App from './app/app';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
