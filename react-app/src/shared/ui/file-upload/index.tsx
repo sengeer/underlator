71c7fde4967d86628276ade48447680aaf450ced
Сергей Поленов <sengeer@GRANITE.local>
2025-02-16 23:31:12 +0300

    feat: add drag-and-drop functionality to FileUpload component

--- react-app/src/shared/ui/file-upload/index.tsx
+++ react-app/src/shared/ui/file-upload/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import './index.scss';
 import CloudIcon from '../../assets/icons/cloud-icon';
 
@@ -9,16 +10,58 @@ function FileUpload({
   isOpened: boolean;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }) {
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleDrag = (e: React.DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDragIn = (e: React.DragEvent) => {
+    handleDrag(e);
+    if (e.dataTransfer.items && e.dataTransfer.items.length > 0) {
+      setIsDragging(true);
+    }
+  };
+
+  const handleDragOut = (e: React.DragEvent) => {
+    handleDrag(e);
+    setIsDragging(false);
+  };
+
+  const handleDrop = (e: React.DragEvent) => {
+    handleDrag(e);
+    setIsDragging(false);
+
+    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+      const syntheticEvent = {
+        target: {
+          files: e.dataTransfer.files,
+        },
+      } as React.ChangeEvent<HTMLInputElement>;
+
+      onChange(syntheticEvent);
+      e.dataTransfer.clearData();
+    }
+  };
+
   return (
     <form
-      className={`file-upload__form ${isOpened ? ' file-upload__form_open' : ''}`}>
+      className={`file-upload__form
+        ${isOpened ? ' file-upload__form_open' : ''}
+        ${isDragging ? ' file-upload__form_dragging' : ''}
+      `}
+      onDragEnter={handleDragIn}
+      onDragLeave={handleDragOut}
+      onDragOver={handleDragIn}
+      onDrop={handleDrop}>
       <label className='file-upload__label' htmlFor='file'>
         <div className='file-upload__design'>
           <CloudIcon />
           <div className='file-upload__text-wrapper'>
-            <p className='file-upload__text'>Перетащить</p>
+            <p className='file-upload__text'>Перетащите</p>
             <p className='file-upload__text'>или</p>
-            <p className='file-upload__text'>Выбрать файл</p>
+            <p className='file-upload__text'>выберите файл</p>
           </div>
         </div>
         <input id='file' type='file' onChange={onChange} />
